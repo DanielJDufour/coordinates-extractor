c43d9ca4c7ef3276f1ecfffd443d249e649aa1c2
danieljdufour <daniel.j.dufour@gmail.com>
2017-04-18 02:28:08 +0000

    Updated capture

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = ['coordinates_extractor'],
   package_dir = {'coordinates_extractor': 'coordinates_extractor'},
   package_data = {'coordinates_extractor': ['__init__.py', 'test/__init__.py', 'tests/test.py']},
-  version = '0.2',
+  version = '0.3',
   description = 'Extract Coordinates from semi-structured text, like Wikipedia xml',
   author = 'Daniel J. Dufour',
   author_email = 'daniel.j.dufour@gmail.com',
